7e6604fba635a695fcdc4f298233e6ee1e5d6aa7
Mostafa Milly <57003058+MostafaKMilly@users.noreply.github.com>
2024-08-03 14:58:33 +0300

    Edit footer

--- src/components/layout/Footer/PaymentMethods.tsx
+++ src/components/layout/Footer/PaymentMethods.tsx
@@ -10,18 +10,18 @@ const PaymentMethods: React.FC = () => {
     {
       src: "https://cdn.builder.io/api/v1/image/assets/TEMP/7ed261ef0a759c2d49039257a1f7b04339850c4625369d49e7b1e9d187fb4dea?apiKey=9810db3822b54ab583e896edd833d595&&apiKey=9810db3822b54ab583e896edd833d595",
       alt: "Mastercard",
-      className: "object-contain shrink-0 self-start w-20",
+      className: "object-contain shrink-0 self-start w-12",
     },
-    { text: "Cash", className: "text-2xl font-semibold text-sky-900" },
+    { text: "Cash", className: "text-2xl font-semibold text-sky-900 flex items-center justify-center" },
     {
       src: "https://cdn.builder.io/api/v1/image/assets/TEMP/a4060590f06773b83ed6d13045499364934b06ac1c0ca9b462387f69211d0727?apiKey=9810db3822b54ab583e896edd833d595&&apiKey=9810db3822b54ab583e896edd833d595",
       alt: "American Express",
-      className: "object-contain shrink-0 self-start w-20",
+      className: "object-contain shrink-0 self-start w-20 my-auto",
     },
   ];
 
   return (
-    <div className="flex flex-wrap gap-5 justify-center text-2xl font-semibold text-sky-900 whitespace-nowrap">
+    <div className="flex flex-wrap gap-5 justify-center text-2xl font-semibold text-sky-900 items-center whitespace-nowrap">
       {paymentMethods.map((method, index) =>
         method.src ? (
           <img
